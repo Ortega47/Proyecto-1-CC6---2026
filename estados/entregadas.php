9194b5b0cefde63e6563a13096c7a5df82a1c3b4
Angel Camargo <angel.camargo@galileo.edu>
2026-09-22 10:34:08 -0600

    Quitar más comentarios

--- estados/entregadas.php
+++ estados/entregadas.php
@@ -1,5 +1,4 @@
 <?php
-    // Pantalla de los envíos en entregadas.
     $estado_fijo = 5;
     $titulo_estado = 'Entregadas';
     require __DIR__ . '/../envios/listado.php';
--- estados/orden_nueva.php
+++ estados/orden_nueva.php
@@ -1,5 +1,4 @@
 <?php
-    // Pantalla de los envíos en orden nueva.
     $estado_fijo = 1;
     $titulo_estado = 'Orden nueva';
     require __DIR__ . '/../envios/listado.php';
